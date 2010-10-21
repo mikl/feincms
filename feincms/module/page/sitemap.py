b948028dc41f2744fb82b2d879b770d22d9dfce5
Mikkel Hoegh <mikkel@hoegh.org>
2010-10-21 18:56:08 +0200

    Removing broken lastmod implementation.

--- feincms/module/page/sitemap.py
+++ feincms/module/page/sitemap.py
@@ -6,9 +6,6 @@ class PageSitemap(Sitemap):
     def items(self):
         return Page.objects.filter(active=True, in_navigation=True)
     
-    def lastmod(self, obj):
-        return obj.modification_date
-    
     # the priority is computed of the depth in the tree of a page
     # may we should make an extension to give control to the user for priority
     def priority(self, obj):
